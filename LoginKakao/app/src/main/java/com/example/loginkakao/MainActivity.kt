5bd65b860a08a41a72e1ad7b99aaf2ffb9e18f99
YoonSuHyeon <96-05-03@naver.com>
2020-01-09 17:56:10 +0900

    LoginKakao SimpleExample

---
+++ LoginKakao/app/src/main/java/com/example/loginkakao/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.loginkakao
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import android.util.Base64.NO_WRAP
+import android.util.Log
+import com.kakao.auth.ISessionCallback
+import com.kakao.auth.Session
+
+import com.kakao.network.ErrorResult
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.MeV2ResponseCallback
+import com.kakao.usermgmt.response.MeV2Response
+import com.kakao.util.exception.KakaoException
+import com.kakao.util.helper.Utility.getPackageInfo
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
+class MainActivity : AppCompatActivity() {
+    private var callback: SessionCallback = SessionCallback()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Session.getCurrentSession().removeCallback(callback);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            Log.d("d", "session get current session")
+            return
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("ggg",getHashKey(this))
+        Session.getCurrentSession().addCallback(callback)
+    }
+
+    fun getHashKey(context: Context): String? {
+        try {
+            if (Build.VERSION.SDK_INT >= 28) {
+                val packageInfo = getPackageInfo(context, PackageManager.GET_SIGNING_CERTIFICATES)
+                val signatures = packageInfo.signingInfo.apkContentsSigners
+                val md = MessageDigest.getInstance("SHA")
+                for (signature in signatures) {
+                    md.update(signature.toByteArray())
+                    return String(Base64.encode(md.digest(), NO_WRAP))
+                }
+            } else {
+                val packageInfo =
+                    getPackageInfo(context, PackageManager.GET_SIGNATURES) ?: return null
+
+                for (signature in packageInfo!!.signatures) {
+                    try {
+                        val md = MessageDigest.getInstance("SHA")
+                        md.update(signature.toByteArray())
+                        return Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+                    } catch (e: NoSuchAlgorithmException) {
+                        Log.d("Dlog.w", "Unable to get MessageDigest. signature=$signature")
+                    }
+                }
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    private class SessionCallback : ISessionCallback {
+        override fun onSessionOpenFailed(exception: KakaoException?) {
+            Log.e("e", "Session Call back :: onSessionOpenFailed: ${exception?.message}")
+        }
+
+        override fun onSessionOpened() {
+            UserManagement.getInstance().me(object : MeV2ResponseCallback() {
+
+                override fun onFailure(errorResult: ErrorResult?) {
+                    Log.d("d", "Session Call back :: on failed ${errorResult?.errorMessage}")
+                }
+
+                override fun onSessionClosed(errorResult: ErrorResult?) {
+                    Log.e("e", "Session Call back :: onSessionClosed ${errorResult?.errorMessage}")
+
+                }
+
+                override fun onSuccess(result: MeV2Response?) {
+                    checkNotNull(result) { "session response null" }
+                    // register or login
+                }
+
+            })
+        }
+
+    }
+}
