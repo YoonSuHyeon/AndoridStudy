70f51d7cbb29547034dd12e59f2691ec061ba36a
YoonSuHyeon <96-05-03@naver.com>
2020-01-31 22:44:28 +0900

    Activity for Popup

---
+++ ShoppingExample4/app/src/main/java/com/example/shoppingexample4/PopupActivity.kt
@@ -0,0 +1,31 @@
+package com.example.shoppingexample4
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.Window
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_popup.*
+
+class PopupActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_popup)
+        val intent = getIntent().getStringExtra("data")
+        txtText.text=intent
+    }
+    fun mOnClose(v: View){
+        finish()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(event?.action==MotionEvent.ACTION_OUTSIDE){
+            return false
+        }
+        return true
+    }
+}
---
+++ ShoppingExample4/app/src/main/java/com/example/shoppingexample4/ShoppingItem.kt
@@ -0,0 +1,5 @@
+package com.example.shoppingexample4
+
+import android.graphics.drawable.Drawable
+
+class ShoppingItem(val image:Drawable,val title:String)
