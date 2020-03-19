8bf857dd981486c8c2550f4494061ff770e9a5e6
Yoon Su Hyeon <96-05-03@naver.com>
2020-03-19 21:30:47 +0900

    CustomAdapterExample

---
+++ CustomAdapterExample/app/src/main/java/com/example/customadapterexample/DriveAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.customadapterexample
+
+import android.content.Context
+import android.widget.ArrayAdapter
+
+class DriveAdapter(context:Context,resId:Int,datas:ArrayList<DriveVO>) : ArrayAdapter<DriveVO>() {
+}
---
+++ CustomAdapterExample/app/src/main/java/com/example/customadapterexample/DriveVO.kt
@@ -0,0 +1,3 @@
+package com.example.customadapterexample
+
+class DriveVO(type:String,title:String,data:String)
