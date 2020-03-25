52312fe00fd1f5bd1fe73a16c990408a191f4809
YoonSuHyeon <96-05-03@naver.com>
2020-03-25 14:57:07 +0900

    ActionbarExample

--- ActionBarExample/app/src/main/java/com/example/actionbarexample/MainActivity.kt
+++ ActionBarExample/app/src/main/java/com/example/actionbarexample/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.actionbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val actionBar: ActionBar? =supportActionBar
+        actionBar?.setDisplayShowHomeEnabled(true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.setIcon(R.drawable.icon)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==android.R.id.home){
+            Toast.makeText(this,"HOME AS UP Click",Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return super.onContextItemSelected(item)
+
     }
 }
