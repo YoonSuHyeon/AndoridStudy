5984c06da0668ab08bb65a9fb10d58f5cc2307f2
Yoon Su Hyeon <96-05-03@naver.com>
2020-09-02 23:57:58 +0900

    텍스트 뷰 , 버튼 클릭

---
+++ DataBindingExample/app/src/main/java/com/example/databindingexample/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.databindingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.databindingexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val nameResult = "kkang"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).myData=this
+
+    }
+    fun onButtonClick(view: View){
+        Toast.makeText(this,"show toast",Toast.LENGTH_SHORT).show()
+    }
+}
